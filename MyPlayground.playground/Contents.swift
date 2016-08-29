0779534e0d1c6d6cdda92df1f5f678230d62d44a
martin <mchibwe01@gmail.com>
2016-08-29 14:45:05 -0700

    switched to Swift3.0

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,3 +3,20 @@
 import UIKit
 
 var str = "Hello, playground"
+
+
+let url = NSURL(string: "http://api.themoviedb.org/3/movie/now_playing")!
+let request = NSMutableURLRequest(URL: url)
+request.addValue("application/json", forHTTPHeaderField: "Accept")
+
+let session = NSURLSession.sharedSession()
+let task = session.dataTaskWithRequest(request) { data, response, error in
+	if let response = response, data = data {
+		print(response)
+		print(String(data: data, encoding: NSUTF8StringEncoding))
+	} else {
+		print(error)
+	}
+}
+
+task.resume()
